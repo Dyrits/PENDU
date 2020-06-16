a4e35a89b566ff6befbea44381d67ba0208507f0
dyrits <dyrits@outlook.fr>
2020-06-16 20:23:48 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pendu
+
+# More about this project
+
+### Related course
+ENI | LE DÉVELOPPEMENT WEB COTE CLIENT 
+TRAVAUX PRATIQUE : Module 8 : Le Language JavaScript
+
+### [Description of the project by ENI](https://github.com/Dyrits/MY-MUSIC/blob/master/Module%2007%20-%20Enonc%C3%A9%20TP%20-%20Application%20My%20Music.pdf)
+
+### Technologies | Libraries | Frameworks | Tools  
+- HTML
+- CSS
+- Sass
+- JavaScript
+
+### Details | Comments
+- This project has been freely made from scratch following global instructions.
+
+### Status
+Completed
+
+#### Last update
+16/06/2020
+
+#### Last update (README.md)
+16/06/2020
---
+++ scripts/script.js
@@ -0,0 +1,84 @@
+let wordToGuess;
+let wordToDisplay;
+let mistakesCount = 0;
+let listOfCorrectLetters = [];
+let drawing = document.querySelector("#drawing");
+const letters = document.querySelector("#letters");
+const word = document.querySelector("#word");
+const status = document.querySelector("#status");
+
+
+startGame()
+drawing.onclick = startGame;
+
+function createButtons() {
+  letters.innerHTML = "";
+  for (let charCode = 65; charCode < 91; charCode++) {
+    let letter = String.fromCharCode(charCode);
+    let buttonLetter = document.createElement("button");
+    buttonLetter.id = `"btn- ${letter}`;
+    buttonLetter.value = letter;
+    buttonLetter.textContent = letter;
+    buttonLetter.onclick = checkLetter;
+    letters.append(buttonLetter);
+  }
+}
+
+function defineWordToGuess() {
+  wordToGuess = dictionnaire[Math.floor(Math.random() * nombreDeMots)]
+}
+
+function displayWordToGuess() {
+  wordToDisplay = String();
+  for (let letter of wordToGuess) {
+      wordToDisplay += listOfCorrectLetters.includes(letter) ? letter  + "  " : "?  "
+  }
+  word.textContent = wordToDisplay;
+}
+
+function checkLetter() {
+  this.setAttribute("disabled", "");
+  if (wordToGuess.includes(this.value)) {
+    listOfCorrectLetters.push(this.value);
+    status.className = "green";
+    status.textContent = `La lettre ${this.value} est bien présente dans le mot!`;
+    document.querySelector("#correct-letters").textContent = `${listOfCorrectLetters.length}`;
+    displayWordToGuess()
+    if (!wordToDisplay.includes("?")) {
+      status.textContent = `La lettre ${this.value} est bien présente dans le mot! Vous survivez en étant à ${6 - mistakesCount} doigt(s) de la mort. `;
+      disableGame();
+    }
+  } else {
+    mistakesCount ++;
+    document.querySelector("#mistakes").textContent = `${mistakesCount}`;
+    drawing.src = `./styles/images/p${mistakesCount}.gif`
+    status.className = "red";
+    status.textContent = `La lettre ${this.value} n'est pas présente dans le mot!`;
+    if (mistakesCount === 6) {
+      status.textContent = "Vous avez perdu ! Vous êtes donc décédé. Profitez-bien de l'au-delà...";
+      disableGame();
+    }
+  }
+}
+
+function disableGame() {
+  for(let letter of letters.children) {
+    letter.setAttribute("disabled", "");
+  }
+}
+
+function startGame() {
+  mistakesCount = 0;
+  document.querySelector("#mistakes").textContent = `${mistakesCount}`;
+  listOfCorrectLetters = [];
+  document.querySelector("#correct-letters").textContent = `${listOfCorrectLetters.length}`;
+  status.className = "white";
+  drawing.src = "./styles/images/p0.gif"
+  status.textContent = "Prêt à mourir ?";
+  createButtons()
+  defineWordToGuess()
+  displayWordToGuess()
+}
+
+
+
